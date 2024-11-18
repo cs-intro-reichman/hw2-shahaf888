f41f8a1747626da97830325d2990c4cf25e4c631
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-18 12:39:01 +0200

    next try calc

--- CalcPi.java
+++ CalcPi.java
@@ -4,7 +4,7 @@ public class CalcPi {
 	    double sumCalc = 1.0;
 		int numOfTimes = Integer.parseInt(args[0]);
 		int sheverToHishuv = 3;
-		for (int i = 1; i <= numOfTimes; i++){
+		for (int i = 1; i < numOfTimes; i++){
 			if (i % 2 == 1){
 				sumCalc = sumCalc - (1.0 / sheverToHishuv);
 				//System.out.println(1.0 / sheverToHishuv);
