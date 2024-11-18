5ae7f0e21f56a82ca99284bc54987bcea6fab8ab
shahaf888 <92173491+shahaf888@users.noreply.github.com>
2024-11-18 12:08:16 +0200

    try include calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,25 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    double sumCalc = 1.0;
+		int numOfTimes = Integer.parseInt(args[0]);
+		int sheverToHishuv = 3;
+		for (int i = 1; i <= numOfTimes; i++){
+			if (i % 2 == 1){
+				sumCalc = sumCalc - (1.0 / sheverToHishuv);
+				//System.out.println(1.0 / sheverToHishuv);
+			}
+			else {
+				sumCalc = sumCalc + (1.0 / sheverToHishuv);
+				//System.out.println(1.0 / sheverToHishuv);
+			}
+			sheverToHishuv = sheverToHishuv + 2;
+			//System.out.println(sheverToHishuv);
+		}
+
+		double piJava = Math.PI;
+		sumCalc = sumCalc * 4.0;
+		System.out.println("pi according to Java: " + piJava);
+		System.out.println("pi, approximated:     " + sumCalc);
 	}
 }
